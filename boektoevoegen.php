fa478a183e831b1a572652d0284b91eafdc64fe8
Aaron Laan <70571116+BoostioAaron@users.noreply.github.com>
2021-02-15 14:31:13 +0100

    Fileinput

--- boektoevoegen.php
+++ boektoevoegen.php
@@ -7,7 +7,7 @@
     <form action="includes/actionboektoevoegen.php" method="post">
 <input type="text" class="Boektoevoegeninputs" id="titelboekid" name="titelboek" placeholder="titelboek..."> <br><br>
 <input type="text" class="Boektoevoegeninputs" id="auteurid" name="auteur" placeholder=" auteur..."> <br><br>
-<input type="text" class="Boektoevoegeninputs" id="boekplaatjeid" name="boekplaatje" placeholder=" boekplaatje..."> <br><br>
+<input type="file" accept="image/png, image/jpeg" class="Boektoevoegeninputs" id="boekplaatjeid" name="boekplaatje" placeholder=" boekplaatje..."> <br><br>
 <textarea placeholder="Boekdescriptie" id="Boekdescriptieid" name="Boekdescriptie" rows="4" cols="50"></textarea> <br>
 <input type="submit" id="Opslaan" name="Opslaan"  value="Opslaan" placeholder=" Boekdescriptie..."> <br>
  </form>
